3fda57ca4cb7d7d0876cfdec86a3cfb84b60f318
RTemiy <RTemiyGo@yandex.ru>
2024-10-26 13:34:09 +0300

    fixed portraits

--- src/Stories/OTV/persons.ts
+++ src/Stories/OTV/persons.ts
@@ -8,7 +8,7 @@ statsManager.add({
   image: require('../../Images/OTV/Persons/Alex_Prologue.png'),
   title: 'Парень, случайно толкнувший меня на концерте.',
   description: 'А он не так прост как кажется. Вначале точными и ловкими движениями удерживает, а затем добродушно ведет беседу и поступает как джентльмен. За уверенной осанкой, спокойной речью и темным взглядом скрывается своя история, и я должна ее разузнать!',
-  showValue: false,
+  showValue: true,
   story: EStoriesEn.OTV
 })
 
@@ -19,6 +19,6 @@ statsManager.add({
   image: require('../../Images/OTV/Persons/Lina_Streetwear.png'),
   title: 'Доброжелательная девушка.',
   description: 'Улыбчивая и миролюбивая брюнетка, и, к тому же, сестра Алекса. Ее отзывчивости нет предела, как и красоте. Посмотрим, что скрывается за миловидным лицом. ',
-  showValue: false,
+  showValue: true,
   story: EStoriesEn.OTV
 })
