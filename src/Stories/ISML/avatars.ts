66e9c8575ba03d7ed611de051a6c566d0598a75a
RTemiy <RTemiyGo@yandex.ru>
2025-12-17 10:10:44 +0300

    updated: pft images deleted: isml

--- src/Stories/ISML/avatars.ts
+++
@@ -1,3 +0,0 @@
-import { achievementsManager } from '../../index'
-import { EStoriesEn } from '../../Utils/EStoriesNames'
-import { profileManager } from '../../Components/Profile/Profile'
--- src/Stories/ISML/choices.ts
+++
@@ -1,9 +0,0 @@
-import { statsManager } from '../../index'
-import { EStoriesEn } from '../../Utils/EStoriesNames'
-
-statsManager.add({
-  category: 'Choice',
-  id: 'DreamForest',
-  idDescription: 'Было в лесу во сне',
-  story: EStoriesEn.ISML
-})
