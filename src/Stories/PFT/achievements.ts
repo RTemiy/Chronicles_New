026c4e26d1dc368f46ff41b34b6ed26913078e50
RTemiy <RTemiyGo@yandex.ru>
2026-03-27 22:05:20 +0300

    updated: PFT prologue

--- src/Stories/PFT/achievements.ts
+++ src/Stories/PFT/achievements.ts
@@ -2,10 +2,26 @@ import { achievementsManager } from '../../index'
 import { EStoriesEn } from '../../Utils/EStoriesNames'
 
 achievementsManager.add({
-  name: 'PrologueCompleted',
-  image: require('../../Media/Images/PFT/Achievements/PrologueCompleted.png'),
-  title: 'Крушение надежд',
-  text: 'Вы нащупали дно. Теперь Вам есть, от чего оттолкнуться',
+  name: 'GreenPlanet',
+  image: require('../../Media/Images/PFT/Achievements/GreenPlanet.png'),
+  title: 'Борец за чистоту планеты',
+  text: 'Вы человек, которому не всё равно',
+  story: EStoriesEn.PFT
+})
+
+achievementsManager.add({
+  name: 'Feministic',
+  image: require('../../Media/Images/PFT/Achievements/Feministic.png'),
+  title: 'Феминистические взгляды',
+  text: 'И правильно. Силу женщины трудно переоценить',
+  story: EStoriesEn.PFT
+})
+
+achievementsManager.add({
+  name: 'GoodMusicFan',
+  image: require('../../Media/Images/PFT/Achievements/GoodMusicFan.png'),
+  title: 'Любитель хорошей музыки',
+  text: 'Когда рэперы и рокеры выясняют отношения, Вы слушаете джаз',
   story: EStoriesEn.PFT
 })
 
@@ -14,5 +30,13 @@ achievementsManager.add({
   image: require('../../Media/Images/PFT/Achievements/FriendshipKeeper.png'),
   title: 'Хранитель дружбы',
   text: 'Друга легче потерять, чем найти',
+  story: EStoriesEn.PFT
+})
+
+achievementsManager.add({
+  name: 'PrologueCompleted',
+  image: require('../../Media/Images/PFT/Achievements/PrologueCompleted.png'),
+  title: 'Крушение надежд',
+  text: 'Вы нащупали дно. Теперь Вам есть, от чего оттолкнуться',
   story: EStoriesEn.PFT
 })
